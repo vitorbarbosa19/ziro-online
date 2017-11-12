643fea0b63f36b7c0201d6c3f5564127d451e2af
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2017-11-12 10:42:23 -0800

    added new brandsand fixed routing for prices page

--- src/components/BrandGallery.js
+++ src/components/BrandGallery.js
@@ -12,6 +12,7 @@ export default class BrandGallery extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      loading: true,
       allPhotos: null
     }
   }
@@ -19,32 +20,47 @@ export default class BrandGallery extends React.Component {
     cloudinaryApi.getBrandGallery(this.props.brand)
       .then((response) => {
         this.setState({
+          loading: false,
           allPhotos: response.data.resources
         })
+      })
+      .catch((error) => {
+        this.setState({
+          loading: false
+        })
+        console.log(error.response)
       })
   }
   render() {
     const whatsappLink = 'whatsapp://send?phone=5511996454922&text=https://res.cloudinary.com/ziro/image/upload/v'
     return (
       <div style={{ textAlign: 'center' }}>
-        {this.state.allPhotos
-			  	? this.state.allPhotos.map((photo, index) => {
-			  		return (
-              <a
-                key={index}
-                href={`${whatsappLink}${photo.version}/${photo.public_id}.jpg?${this.props.brand.replace(/\s/, '-')}`}>
-                <Image
-                  style={imageStyle}
-                  cloudName='ziro'
-                  width='400'
-                  publicId={photo.public_id}
-                  format='jpg'
-                  secure='true'
-            	/>
-              </a>
-				  	)
-			  	})
-          :	<Spinner />
+        {this.state.loading
+			  	? <Spinner />
+          : this.state.allPhotos
+            ? this.state.allPhotos.map((photo, index) => {
+    			  		return (
+                <a
+                  key={index}
+                  href={`${whatsappLink}${photo.version}/${photo.public_id}.jpg?${this.props.brand.replace(/\s/, '-')}`}>
+                  <Image
+                    style={imageStyle}
+                    cloudName='ziro'
+                    width='400'
+                    publicId={photo.public_id}
+                    format='jpg'
+                    secure='true'
+                	/>
+                </a>
+    				  	)
+  			  	  })
+            :
+            <div style={{
+              margin: '20px 40px',
+              fontFamily: 'karla'
+            }}>
+                Ainda sem fotos para esta marca. Volte em breve!
+            </div>
         }
       </div>
     )
