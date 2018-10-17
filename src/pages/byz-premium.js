1ee4a96b4850858697c822498e570febcf9c7a90
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-10-16 21:23:01 -0700

    remove brands that are out of operation

--- src/pages/byz-premium.js
+++ src/pages/byz-premium.js
@@ -2,5 +2,5 @@ import React from 'react'
 import BrandGallery from '../components/BrandGallery'
 
 export default () => (
-  <BrandGallery brand='Banana Co' />
+  <BrandGallery brand='Byz Premium' />
 )
