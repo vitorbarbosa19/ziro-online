86746310ef481e0ab5538950c89431125481ef0b
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-01-22 09:37:50 -0800

    update brands

--- src/pages/sara-mari.js
+++ src/pages/sara-mari.js
@@ -2,5 +2,5 @@ import React from 'react'
 import BrandGallery from '../components/BrandGallery'
 
 export default () => (
-  <BrandGallery brand='Sara & Mari' />
+  <BrandGallery brand='Sara Mari' />
 )
